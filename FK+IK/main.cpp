272822fac87fba4d365cbdba9b73d43d9d112e26
sarang nair <sarangnair@sarangs-MacBook-Air.local>
2017-07-22 09:22:10 +0530

    Initial Commit

---
+++ FK+IK/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FK+IK
+//
+//  Created by sarang nair on 31/04/1939 Saka.
+//  Copyright © 1939 Saka sarang_nair. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
